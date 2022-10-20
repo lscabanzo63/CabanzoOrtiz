7715f69e82ae382f8639fb01f0d7953745e80b82
sala4 <sala4@is-ls04e17.FI-LabInf.ucatolica.loc>
2022-10-20 13:45:23 -0500

    Version 1.0 Hasta el tiempo estblecido en clase

---
+++ src/main/java/com/mycompany/cabanzosantiago/CabanzoSantiago.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.cabanzosantiago;
+
+/**
+ *
+ * @author sala4
+ */
+public class CabanzoSantiago {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
